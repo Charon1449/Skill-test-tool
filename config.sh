cb9b1d954c3d1d127cd8c41b0591960373b3dfa9
eugenes <eugenes@kraken.kraken>
2020-11-04 22:24:31 +0000

    first config creation

---
+++ config.sh
@@ -0,0 +1,9 @@
+#creating the github repo
+mkdir scrapping-test-{condidate_name}
+cd scrapping-test-{condidate_name}
+git init
+
+#get repo status
+git status
+#add to stagging state
+git add {file_name} / or git add . #to add all the files
